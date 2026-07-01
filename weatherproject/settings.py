15a29d01f3716fb6010fa9787de5b37ec4f8f440
Polina <polinswxcz@gmail.com>
2026-07-01 22:26:57 +0300

    fix allowed hosts

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -14,12 +14,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config(
-    ALLOWED_HOSTS = ["*"],
-    default="127.0.0.1,localhost",
-    cast=lambda v: [s.strip() for s in v.split(",")]
-)
-
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
